fdde91b1091c9054cb37d86375a8687a476e421b
jaejin <jaejin960729@gmail.com>
2021-08-08 23:32:55 +0900

    .

--- Simulator/Simulator/simglucose/controller/dqnlearnctrller.py
+++ Simulator/Simulator/simglucose/controller/dqnlearnctrller.py
@@ -41,6 +41,7 @@ class DQNController(Controller):
         self.time = 0
 
         self.name = 'dqn'
+        self.modelName = ''
 
 
         # 리플레이 메모리
@@ -48,6 +49,7 @@ class DQNController(Controller):
         self.bgInsulinMemory = deque(maxlen=self.previous_time)
         self.memory = deque(maxlen=30000)
 
+        self.load_model = False
         # 모델과 타깃 모델 생성
         if model == 'g':
             self.model = self.build_gru_model()
@@ -55,12 +57,21 @@ class DQNController(Controller):
             self.epsilon = 0.05
             self.learning_rate = 0.00001
             self.epsilon_min = 1
+            self.modelName = 'g'
+            if self.load_model:
+                self.model.load_weights("g.h5")
+                print("load")
+
         elif model == 'c':
             self.model = self.build_cnn_model()
             self.target_model = self.build_cnn_model()
             self.epsilon = 0.05
             self.learning_rate = 0.001
             self.epsilon_min = 1
+            self.modelName = 'c'
+            if self.load_model:
+                self.model.load_weights("c.h5")
+                print("load")
         else:
             self.model = self.build_model()
             self.target_model = self.build_model()
@@ -76,11 +87,9 @@ class DQNController(Controller):
         # self.target = target
         self.onetimetest = 1
 
-        self.load_model = True
 
-        if self.load_model:
-            self.model.load_weights("teest.h5")
-            print("load")
+
+
 
     def reset(self, obs, reward, done, info):
         name = info.get('patient_name')
@@ -106,7 +115,7 @@ class DQNController(Controller):
         model.add(Dense(self.action_size, activation='linear',
                         kernel_initializer='he_uniform'))
         model.summary()
-        model.compile(loss='mse', optimizer=Adam(lr=self.learning_rate))
+        model.compile(loss='huber', optimizer=Adam(lr=self.learning_rate))
         return model
 
     def build_gru_model(self):
@@ -219,6 +228,153 @@ class DQNController(Controller):
                 target[i][actions[i]] = rewards[i] + self.discount_factor * (np.amax(target_val[i]))
 
         self.model.fit(np.reshape(states, (self.batch_size, 48, 2)), target, batch_size=self.batch_size,
-                       epochs=80, verbose=0)
+                       epochs=80, verbose=1)
 
+
+class DqnPredController(Controller):
+    def __init__(self, state_size, action_size, episode, previous_time, model):
+        self.state_size = state_size
+        self.action_size = action_size
+        self.episode = episode
+        self.quest = pd.read_csv(CONTROL_QUEST)
+        self.patient_params = pd.read_csv(PATIENT_PARA_FILE)
+
+        self.actions = 0
+        self.learning_rate = 0.001
+        self.discount_factor = 0.99
+        self.epsilon = 0.2
+        self.epsilon_min = 0.01
+        self.batch_size = 128
+        self.train_start = 128
+        self.basal = 0
+        self.epsilon_decay = 0.9992
+        self.previous_time = previous_time * 12
+
+        self.time = 0
+
+        self.name = 'dqn'
+
+
+        # 리플레이 메모리
+
+        self.bgInsulinMemory = deque(maxlen=self.previous_time)
+        self.memory = deque(maxlen=30000)
+
+        # 모델과 타깃 모델 생성
+        if model == 'g':
+            self.model = self.build_gru_model()
+            self.target_model = self.build_gru_model()
+            self.epsilon = 0.05
+            self.learning_rate = 0.00001
+            self.epsilon_min = 1
+        elif model == 'c':
+            self.model = self.build_cnn_model()
+            self.target_model = self.build_cnn_model()
+            self.epsilon = 0.05
+            self.learning_rate = 0.001
+            self.epsilon_min = 1
+        else:
+            self.model = self.build_model()
+            self.target_model = self.build_model()
+
+
+        # 타깃 모델 초기화
+        self.update_target_model()
+
+        # 환자 정보
+        self.quest = pd.read_csv(CONTROL_QUEST)
+        self.patient_params = pd.read_csv(PATIENT_PARA_FILE)
+
+        # self.target = target
+        self.onetimetest = 1
+
+        self.load_model = True
+
+        if self.load_model:
+            self.model.load_weights("teest.h5")
+            print("load")
+
+    def reset(self, obs, reward, done, info):
+        name = info.get('patient_name')
+
+        if any(self.quest.Name.str.match(name)):
+            params = self.patient_params[self.patient_params.Name.str.match(
+                name)]
+            u2ss = params.u2ss.values.item()  # unit: pmol/(L*kg)
+            BW = params.BW.values.item()      # unit: kg
+        else:
+            u2ss = 1.43   # unit: pmol/(L*kg)
+            BW = 57.0     # unit: kg
+
+        self.basal = u2ss * BW / 6000  # unit: U/min
+
+
+    def build_model(self):
+
+        model = Sequential()
+        model.add(Dense(24, input_dim=self.previous_time, activation='relu',
+                        kernel_initializer='he_uniform'))
+        model.add(Dense(24, activation='relu',
+                        kernel_initializer='he_uniform'))
+        model.add(Dense(self.action_size, activation='linear',
+                        kernel_initializer='he_uniform'))
+        model.summary()
+        model.compile(loss='huber', optimizer=Adam(lr=self.learning_rate))
+        return model
+
+    def build_gru_model(self):
+
+        model = Sequential()
+        model.add(Dense(128, input_shape=(self.previous_time*self.state_size, 2),
+                        activation='relu', kernel_initializer='he_uniform'))
+        model.add(GRU(128, return_sequences=True))
+        # model.add(GRU(128, return_sequences=True))
+        model.add(Flatten())
+        model.add(Dense(self.action_size, activation='softmax', kernel_initializer='he_uniform'))
+        model.summary()
+        model.compile(loss='mse', optimizer=Adam(lr=self.learning_rate))
+        return model
+
+    def build_cnn_model(self):
+
+        model = Sequential()
+        print(self.previous_time)
+        model.add(Conv1D(32, kernel_size=3, activation='relu',
+                         input_shape=(48, 2)))
+        model.add(MaxPooling1D(pool_size=2))
+        model.add(Conv1D(32, kernel_size=3, activation='relu'))
+        model.add(MaxPooling1D(pool_size=2))
+        model.add(Dense(512, activation='relu',
+                        kernel_initializer='he_uniform'))
+        model.add(Dropout(0.2))
+        model.add(Flatten())
+        model.add(Dense(self.action_size, activation='softmax',
+                        kernel_initializer='he_uniform'))
+        model.summary()
+        model.compile(loss='mse', optimizer=Adam(lr=self.learning_rate))
+        return model
+
+    def update_target_model(self):
+        self.target_model.set_weights(self.model.get_weights())
+
+    def policy(self, state):
+        r = 0
+        if np.random.rand() <= self.epsilon:
+            r = random.randrange(self.action_size)
+        else:
+            # print("h ", np.reshape(list(self.BGmemory),[1,self.previous_time]))
+            # np.reshape(states, (4, 48, 1))
+            q_value = self.model.predict(np.reshape(list(self.bgInsulinMemory), (1, 48, 2)))
+            # print("q: ", q_value, "shape", q_value.shape)
+            r = np.argmax(q_value[0])
+            # print("R ", r)
+        return r
+
+    def get_basal(self, r):
+        if r == 0:
+            return Action(basal=0, bolus=0)
+        elif r == 1:
+            return Action(basal=self.basal, bolus=0)
+        elif r == 2:
+            return Action(basal=self.basal * 5, bolus=0)
 
